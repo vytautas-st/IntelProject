99c6e19c2f510d562111f348111f9a7e4127f923
Vytautas_St <vytautas.stockus@gmail.com>
2022-08-24 14:53:08 +0300

    Vsiems html padarytas internacionalizavimas

---
+++ 2.IntelProject/src/main/java/lt/codeacademy/AppConfiguration.java
@@ -0,0 +1,38 @@
+package lt.codeacademy;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class AppConfiguration implements  WebMvcConfigurer {
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+	    SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.US);
+	    System.out.println(" veikia localeResolver");
+	    return slr;
+	}
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	    lci.setParamName("lang");
+	    System.out.println("veikia localeChangeInterceptor");
+	    return lci;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(localeChangeInterceptor());
+	    System.out.println(" veikia override");
+	}
+
+
+}
